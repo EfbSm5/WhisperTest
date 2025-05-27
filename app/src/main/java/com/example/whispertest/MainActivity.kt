e9442c3b72786eeb704ad9ffd5248e1622bf044b
EfbSm5 <3325989547@qq.com>
2025-05-27 21:35:22 +0800

    update

--- app/src/main/java/com/example/whispertest/MainActivity.kt
+++ app/src/main/java/com/example/whispertest/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.whispertest
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.whispertest.databinding.ActivityMainBinding
@@ -18,15 +21,28 @@ class MainActivity : AppCompatActivity() {
     private val DEFAULT_WAV: String = "english_test2.wav"
     private lateinit var viewBinding: ActivityMainBinding
     private val audioUtil = VoiceRecorder()
+    val recordBtn = Button(AppUtil.getContext())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initUi()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        audioUtil.stop()
+    }
+
+    fun initUi() {
+        recordBtn.text = getString(R.string.record)
+        recordBtn.id = View.generateViewId()
         viewBinding.translate.setOnClickListener {
             Toast.makeText(this, "click", Toast.LENGTH_SHORT).show()
             try {
@@ -51,7 +67,7 @@ class MainActivity : AppCompatActivity() {
                 throw e
             }
         }
-        viewBinding.record.setOnClickListener {
+        recordBtn.setOnClickListener {
             Toast.makeText(this, "record", Toast.LENGTH_SHORT).show()
             AppUtil.checkPermission(this)
             try {
@@ -60,11 +76,52 @@ class MainActivity : AppCompatActivity() {
                 throw e
             }
         }
+        val items = listOf(DEFAULT_WAV, "inputVoice")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        viewBinding.spinner.adapter = adapter
+        viewBinding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                if (items[position] == "inputVoice") {
+                    changeView(true)
+                } else {
+                    changeView(false)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+        }
+
     }
 
-    override fun onStop() {
-        super.onStop()
-        audioUtil.stop()
+    fun changeView(showRecord: Boolean) {
+        val layout = viewBinding.main
+        val layoutSet = ConstraintSet()
+        layoutSet.clone(layout)
+        val playBtn = viewBinding.play
+        val tranBtn = viewBinding.translate
+        if (showRecord) {
+            layout.addView(recordBtn)
+            layoutSet.clear(playBtn.id, ConstraintSet.END)
+            layoutSet.clear(tranBtn.id, ConstraintSet.START)
+            layoutSet.connect(playBtn.id, ConstraintSet.END, recordBtn.id, ConstraintSet.START)
+            layoutSet.connect(recordBtn.id, ConstraintSet.START, playBtn.id, ConstraintSet.END)
+            layoutSet.connect(recordBtn.id, ConstraintSet.END, tranBtn.id, ConstraintSet.START)
+            layoutSet.connect(tranBtn.id, ConstraintSet.START, recordBtn.id, ConstraintSet.END)
+        } else {
+            layoutSet.connect(playBtn.id, ConstraintSet.END, tranBtn.id, ConstraintSet.START)
+            layoutSet.connect(tranBtn.id, ConstraintSet.START, recordBtn.id, ConstraintSet.END)
+            layoutSet.clear(recordBtn.id, ConstraintSet.START)
+            layoutSet.clear(recordBtn.id, ConstraintSet.END)
+            layout.removeView(recordBtn)
+
+        }
+        layoutSet.applyTo(layout)
     }
 
 }
